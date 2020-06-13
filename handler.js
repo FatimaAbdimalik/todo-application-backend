9a67f9f9f98bdc1df221a730d7aa8ff6c8e62fe8
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-06-13 07:40:17 +0100

    done

--- handler.js
+++ handler.js
@@ -1,18 +1,53 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { response } = require("express");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/tasks", function (req, res) {
+  res.status(200).send({
+    tasks: [
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        id: 1,
+        text: ["water plants", "do dishes", "buy oats"],
+        status: 0,
+        completed: false,
       },
-      null,
-      2
-    ),
-  };
+    ],
+  });
+});
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.tasks = serverless(app);
+
+app.post("/tasks", function (req, res) {
+  res.status(201).send({
+    text: "buy cat fod",
+    completed: false,
+    date: "2019-10-07",
+  });
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.json({
+    message: `Received a request to add task ${text} with date ${date}`,
+  });
+});
+
+app.put("/tasks/:id", function (req, res) {
+  const id = req.params.id;
+  const data = req.body;
+  res.status(200).send(`Update task ${id} and its ${data}`);
+});
+
+app.delete("/tasks/:id", function (req, res) {
+  const id = req.params.id;
+  const someResponse = {
+    message: "You issued a delete request for ID",
+  };
+  res.status(200).send(someResponse);
+});
